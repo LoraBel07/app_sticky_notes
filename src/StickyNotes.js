d1e0943ebac6ca1ce7df3a5453be713ea198120b
LoraBel07 <91973134+LoraBel07@users.noreply.github.com>
2022-01-09 21:37:09 -0500

    commit

--- src/StickyNotes.js
+++ src/StickyNotes.js
@@ -8,11 +8,7 @@ export class StickyNotes extends Component {
 		
 	state = {
 		typeNote: '',
-		notesSet: [],
-		item: [],
-		count: this.props.value,
-		items: [{ id: 1, value: 0 }, { id: 2, value: 10 }, { id: 3, value: 0 }]
-		
+		notesSet: []		
 	}
 	addClassName() {
 		this.typeNote.addClassName('removed')	
@@ -37,13 +33,7 @@ export class StickyNotes extends Component {
 		listArray = [];
 		this.setState({notesSet: listArray});		
 	}
-	// handleDelete = itemId => {
-	// 	alert("Button Clicked!");
-	// const items = this.state.items.filter(item => item.id !== itemId);
-	// this.setState({ items: items });
-	// };
-
-			
+	
 	onChangeEvent(e) {
 		this.setState({typeNote: e});
 	}
@@ -56,7 +46,11 @@ export class StickyNotes extends Component {
 		const li = event.target;
 		li.classList.toggle('crossed');
 	}
-		
+	deleteOneItem(e) {
+		let notesArray = this.state.notesSet;
+        notesArray.splice(e.index, 1);
+        this.setState({notesSet: notesArray});
+	};		
 	render() {
 		return(
 			<form onSubmit={ this.onFormSubmit }>
@@ -66,30 +60,19 @@ export class StickyNotes extends Component {
 								placeholder="Write Note ..."								
 								onChange={ (e) => { this.onChangeEvent(e.target.value) } }
 								value={ this.state.typeNote } />
-								<button onClick={() => this.addItem(this.state.typeNote)}><img className="onIcon" src={onIcon}  alt="on icon" width="30px"/></button>
-								<button onClick={() => this.deleteItem()}><img className="offIcon" src={offIcon} alt="off icon" width="30px"/></button>
+								<button onClick={ () => this.addItem(this.state.typeNote) }><img className="onIcon" src={ onIcon }  alt="on icon" width="30px"/></button>
+								<button onClick={ () => this.deleteItem() }><img className="offIcon" src={ offIcon } alt="off icon" width="30px"/></button>
 					</div>
-					<div className="containerNotes">
-											
-							{this.state.notesSet.map((item, index) => (
-								<p id="note" onClick={ this.crossedWord } onDblClick={ this.deleteNote } key={index}>
+					<div className="containerNotes">											
+							{ this.state.notesSet.map((item, index) => (
+								<p id="note" onClick={ this.crossedWord } onDoubleClick={ (e) => this.deleteOneItem({index}) } key={index}>
 									<img className="pin" src={pin} alt="pin" width="85px"/>
-									{item}
-									{/* <button className="offIconNote" onClick={() => this.props.onDelete(this.props.id)}>
-									<img src={offIcon} alt="off icon" width="30px"/>
-									</button> */}
-									
+									{item}									
 									</p>
-							))}	
+							)) }	
 											
-						
-				
 					</div>
-
-
-				</form>		
-			
-			
+			</form>	
 		)
 	}
 } 
