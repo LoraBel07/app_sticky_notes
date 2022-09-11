69bd49fa6912a5d8e33cf328d70744809befa52c
LoraBel07 <91973134+LoraBel07@users.noreply.github.com>
2022-09-10 20:42:47 -0400

    commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
+import { createContext, useState } from 'react';
 import './App.css';
 import Btnback from './Btnback';
 import { StickyNotes } from './StickyNotes';
 
+export const ThemeContext = createContext("light");
+
+
+
 function App() {
+  const [ theme, setTheme] = useState("light");
+
+  const toggleTheme = () => {
+    setTheme((curr) => (curr === "light" ? "dark" : "light"));
+  };
   return (
-    
-    <div className="app">
-          
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+    <div className="app" id={theme}>         
       <StickyNotes />
       <Btnback />  
     </div>
+    </ThemeContext.Provider>
   );
 }
 
